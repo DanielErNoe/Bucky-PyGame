f389d02c7b31da7d93710faddf4039d7b4a1525e
Daniel Ervik Riiber <danerriiber@gmail.com>
2023-05-30 11:54:02 +0200

    Simplified and optimized the data processing code for values

--- ControllerSerial.py
+++ ControllerSerial.py
@@ -7,7 +7,8 @@ for port in ports: print(port)
 
 ser = serial.Serial('COM9', 100)
 
-dead_zone = 25
+DEAD_ZONE = 25
+RES_OFFSET = 512 #1023/2
 
 #reset the arduino
 ser.setDTR(False)
@@ -15,27 +16,19 @@ time.sleep(1)
 ser.flushInput()
 ser.setDTR(True)
 
-def readData():
+def read_data():
     kmax = 1
     for k in range(kmax):
         try:
-            data = ser.readline()
-            data = data.decode().rstrip()
-            Verdier = data.split('x')[1].split('y')
-            Verdier.append(int(data.split('x')[0][1:]))
-            #print(data)
-
-            Verdier[0], Verdier[1] = int(Verdier[0]) - 1023 // 2, int(Verdier[1]) - 1023 // 2
-
-            Verdier[0] = 0 if Verdier[0] < dead_zone and Verdier[0] > -dead_zone else Verdier[0]
-            Verdier[1] = 0 if Verdier[1] < dead_zone and Verdier[1] > -dead_zone else Verdier[1]
-
-            Verdier[0], Verdier[1] = Verdier[0] * 2, Verdier[1] * 2
+            data = ser.readline().decode().rstrip()
+            
+            values = list(map(int, data[1:].replace('x', ' ').replace('y', ' ').split()))
+            values = [x - RES_OFFSET for x in values[-2:]]
+            x_value, y_value, b_value = [0 if abs(x) < DEAD_ZONE else x for x in values] + [int(data[1:].split('x')[0])]
+            values = [x_value, y_value, b_value]
 
         except:
             print("Serial connection lost. Reconnecting...")
             ser.close() # close the serial port
             ser.open() # reopen the serial port 
-    return Verdier
-
-print(readData())
+    return values
