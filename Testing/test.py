53adb9b069ccab154d78ea2d19e376e7db62d220
Daniel Ervik Riiber <danerriiber@gmail.com>
2023-05-30 18:19:07 +0200

    added a .gitignore

--- Testing/test.py
+++ Testing/test.py
@@ -9,3 +9,20 @@ v1 = func(10); v2 = func(12)
 print(v1, v2)
 
 print(func(10))
+
+data = "b1x532y495"
+verdier = list(map(int, data[1:].replace('x', ' ').replace('y', ' ').split()))
+#verdier[0], verdier[1] = int(verdier[0]) - 512, int(verdier[1]) - 512
+verdier = [x - 512 for x in verdier[-2:]]
+
+#x = 0 if abs(10) < 20 else "wow"
+print(verdier)
+#x_verdier, y_verdi = [0 if abs(x) < 20 else x for x in verdier[-2:]]
+#print(x_verdier, y_verdi)
+#print(verdier[:2])
+x_verdi, y_verdi, b_verdi = [0 if abs(x) <= 20 else x for x in verdier] + [int(data[1:].split('x')[0])]
+print(x_verdi, y_verdi, b_verdi)
+
+
+#print(verdier)
+#print(data[1:].split('x')[0])
